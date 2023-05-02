88918fcfabd716f9d437645cc7f7453d4497be3d
Jhoseph Poma <japoma_200@hotmail.com>
2023-05-02 18:14:26 -0500

    HomePage reworked

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { drinks, healthy, lunch, shrimp } from './assets/popular/popularFiles';
 import DynamicBg from './components/DynamicBg';
 import LinkTo from './components/LinkTo';
 import WordLoop from './components/WordLoop';
+import Arrow from './components/icons/Arrow';
 import Fork from './components/icons/Fork';
 
 const word_list = ['Fried', 'Baked', 'Steamed', 'Boiled'];
@@ -14,8 +16,8 @@ const bgList = [
 const App = () => {
 	return (
 		<main>
-			<section className='relative'>
-				<DynamicBg bglist={bgList} />
+			<section className='relative h-screen w-full overflow-hidden'>
+				<DynamicBg bglist={bgList} opacitylvl={0.6} />
 				<div className='absolute left-1/2 top-1/2 flex w-full max-w-7xl -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-8 sm:gap-6'>
 					<h1 className='flex justify-center text-6xl font-bold text-orange-700 sm:text-7xl'>
 						<span className='opacity-0'>&nbps;</span>
@@ -37,8 +39,53 @@ const App = () => {
 					</LinkTo>
 				</div>
 			</section>
+			<section className='relative'>
+				<div
+					className='absolute h-[50vh] w-full bg-black bg-cover bg-center bg-no-repeat'
+					style={{ backgroundImage: 'url("/src/assets/popularcat.png")' }}
+				/>
+				<article className='relative z-50 mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 pt-[200px] text-center text-white'>
+					<div className='text-lg font-bold tracking-wider'>FEATURE</div>
+					<div className='text-5xl'>Popular Categories</div>
+					<div className='mx-auto max-w-3xl text-lg'>
+						Browse our popular categories and find culinary inspiration in one place. From classic
+						dishes to gastronomic trends.
+					</div>
+					<ul className='flex flex-wrap justify-evenly gap-8'>
+						{POP_CATEGORIES.map(category => (
+							<li
+								key={category.id}
+								className='group relative h-60 w-60 overflow-hidden rounded-full border-8 border-white'
+							>
+								<div
+									style={{ backgroundImage: `url(${category.url})` }}
+									className='absolute h-56 w-56 rounded-full bg-cover transition-all duration-500 group-hover:scale-125'
+								>
+									<div className='h-56 w-56 rounded-full bg-black opacity-50 transition-all duration-500 group-hover:opacity-20' />
+								</div>
+								<LinkTo
+									to='recipes'
+									className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-base text-white'
+								>
+									<div className='flex items-center gap-1 rounded-lg bg-orange-600 px-2 py-1'>
+										<span className='whitespace-nowrap'>Discover Recipes</span>
+										<Arrow className='h-5' />
+									</div>
+								</LinkTo>
+							</li>
+						))}
+					</ul>
+				</article>
+			</section>
 		</main>
 	);
 };
+
+const POP_CATEGORIES = [
+	{ id: 1, url: drinks },
+	{ id: 2, url: healthy },
+	{ id: 3, url: lunch },
+	{ id: 4, url: shrimp }
+];
 
 export default App;
